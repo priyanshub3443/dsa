9f75145b96a0662b09e1c060dd6225aad9fce61c
priyanshu bharti <110742345+priyanshub3443@users.noreply.github.com>
2024-02-08 22:02:58 +0530

    Create stackpuch.c

---
+++ dsa/stackpuch.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct stack
+{
+    int size;
+    int top;
+    int *arr;
+};
+
+int isEmpty(struct stack *ptr)
+{
+    if (ptr->top == -1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+int isFull(struct stack *ptr)
+{
+    if (ptr->top == ptr->size - 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+void push(struct stack *ptr, int val)
+{
+    if (isFull(ptr))
+    {
+        printf("stacl overflow / can not push %d is stack", val);
+    }
+    else
+    {
+        ptr->top++;
+        ptr->arr[ptr->top] = val;
+    }
+}
+int pop(struct stack *ptr)
+{
+    if (isEmpty(ptr))
+    {
+        printf("stack underflow / can not pop is stack");
+        return -1;
+    }
+    else
+    {
+        int val = ptr->arr[ptr->top];
+        ptr->top--;
+        return val;
+    }
+}
+
+int main()
+{
+    struct stack *sp = (struct stack *)malloc(sizeof(struct stack));
+    sp->size = 10;
+    sp->top = -1;
+    sp->arr = (int *)malloc(sp->size * sizeof(int));
+    printf("Stack is create sesssesfuly \n");
+    push(sp, 23);
+    push(sp, 99);
+    push(sp, 75);
+    push(sp, 3);
+    push(sp, 64);
+    push(sp, 57);
+    push(sp, 46);
+    push(sp, 89);
+    push(sp, 69);
+    push(sp, 6); // ---> Pushed 10 values
+    push(sp, 46); // Stack Overflow since the size of the stack is 10
+    printf("After pushing, Full: %d\n", isFull(sp));
+    printf("After pushing, Empty: %d\n", isEmpty(sp));
+}
