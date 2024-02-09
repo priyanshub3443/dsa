11285c330d6fae2acbfc08d45837f2651aa3ecc6
priyanshu bharti <110742345+priyanshub3443@users.noreply.github.com>
2024-02-09 23:03:39 +0530

    Update linklist_stack.c

--- dsa/linklist_stack.c
+++ dsa/linklist_stack.c
@@ -1,5 +1,5 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 struct Node
 {
@@ -7,27 +7,79 @@ struct Node
     struct Node *next;
 };
 
-int isEmpty(struct node *top){
-    if (top==NULL)
+void linkedListTraversal(struct Node *ptr)
+{
+    while (ptr != NULL)
+    {
+        printf("Element: %d\n", ptr->data);
+        ptr = ptr->next; 
+    }
+}
+
+
+int isEmpty(struct Node *top)
+{
+    if (top == NULL)
     {
         return 1;
     }
-    else {
+    else
+    {
         return 0;
     }
 }
-int isFull(struct node *top){
-    struct Node* p = (struct Node*)malloc(sizeof(struct Node));
-    if (p==NULL)
+int isFull(struct Node *top)
+{
+    struct Node *p = (struct Node *)malloc(sizeof(struct Node));
+    if (p == NULL)
     {
         return 1;
     }
-    else {
+    else
+    {
         return 0;
     }
 }
 
-int main(){
-    struct node *top=NULL;
+struct Node *push(struct Node *top, int x)
+{
+    if (isFull(top))
+    {
+        printf("this stack is overflow\n");
+    }
+    else
+    {
+        struct Node *n = (struct Node *)malloc(sizeof(struct Node));
+        n->data = x;
+        n->next = top;
+        top = n;
+        return top;
+    }
+}
+int pop(struct Node** top){
+    if(isEmpty(*top)){
+        printf("Stack Underflow\n");
+    }
+    else{
+        struct Node* n = *top;
+        *top = (*top)->next;
+        int x = n->data;
+        free(n);
+        return x; 
+    }
+}
+
+
+int main()
+{
+    struct Node *top = NULL;
+    top = push(top, 78);
+    top = push(top, 7);
+    top = push(top, 8);
+
+    linkedListTraversal(top);
+        int element = pop(&top);
+    printf("Popped element is %d\n", element);
+    linkedListTraversal(top);
     return 0;
 }
