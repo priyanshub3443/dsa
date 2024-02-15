38eb291294e3cf56676bf2344ff4a1c0d5603983
priyanshu bharti <110742345+priyanshub3443@users.noreply.github.com>
2024-02-15 21:55:20 +0530

    circurlar

---
+++ dsa/circular_queue.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct circular_queue
+{
+    int size;
+    int r;
+    int f;
+    int *arr;
+};
+int isEmpty(struct circular_queue *q)
+{
+    if (q->r == q->f)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+int isFull(struct circular_queue *q)
+{
+    if ((q->r + 1) % q->size == q->f)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+void enqueue(struct circular_queue *q, int val)
+{
+    if (isFull(q))
+    {
+        printf("THis queue is overflow");
+    }
+    else
+    {
+        q->r = (q->r + 1) % q->size;
+        q->arr[q->r] = val;
+        printf("Enqued element: %d\n", val);
+    }
+}
+int dequeue(struct circular_queue *q)
+{
+    int a = -1; if (isEmpty(q))
+    {
+        printf("THis queue is empaty");
+    }
+    else
+    {
+        q->f = (q->f + 1) % q->size;
+        a = q->arr[q->f];
+    }
+    return a;
+}
+int main()
+{
+    struct circular_queue q;
+    q.size = 4;
+    q.f = q.r = 0;
+    q.arr = (int *)malloc(q.size * sizeof(int));
+
+    // Enqueue few elements
+    enqueue(&q, 12);
+    enqueue(&q, 15);
+    enqueue(&q, 1);
+    printf("Dequeuing element %d\n", dequeue(&q));
+    printf("Dequeuing element %d\n", dequeue(&q));
+    printf("Dequeuing element %d\n", dequeue(&q));
+    enqueue(&q, 45);
+    enqueue(&q, 45);
+    enqueue(&q, 45);
+
+    if (isEmpty(&q))
+    {
+        printf("Queue is empty\n");
+    }
+    if (isFull(&q))
+    {
+        printf("Queue is full\n");
+    }
+
+    return 0;
+}
