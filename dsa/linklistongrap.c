4535df58ea73ec3ea29d8e3e917946109a39d7f9
priyanshu bharti <110742345+priyanshub3443@users.noreply.github.com>
2024-02-22 23:59:46 +0530

    grap

--- dsa/linklistongrap.c
+++ dsa/linklistongrap.c
@@ -27,6 +27,26 @@ void inorderTraversal(struct node *root)
         inorderTraversal(root->right);
     }
 }
+void preoder(struct node *root)
+{
+    if (root != NULL)
+    {
+        // inoder treverser
+        printf("%d ", root->data);
+        preoder(root->left);
+        preoder(root->right);
+    }
+}
+void postorder(struct node *root)
+{
+    if (root != NULL)
+    {
+        // inoder treverser
+        postorder(root->left);
+        postorder(root->right);
+        printf("%d ", root->data);
+    }
+}
 
 int main()
 {
@@ -55,19 +75,22 @@ int main()
     */
 
     // Constructing the root node - Using Function (Recommended)
-    struct node *p = createNode(2);
+    struct node *p = createNode(4);
     struct node *p1 = createNode(1);
     struct node *p2 = createNode(6);
     struct node *p3 = createNode(5);
     struct node *p4 = createNode(2);
-   
 
     // Linking the root node with left and right children
     p->left = p1;
     p->right = p2;
-    p1->left=p3;
-    p1->right=p4;
-    printf("Inorder Traversal: ");
-   inorderTraversal(p);
+    p1->left = p3;
+    p1->right = p4;
+    preoder(p);
+    printf("prerder Traversal: \n");
+    inorderTraversal(p);
+    printf("Inorder Traversal: \n");
+    postorder(p);
+    printf("postorder Traversal: \n");
     return 0;
 }
