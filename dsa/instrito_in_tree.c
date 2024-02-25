787da6a43ce31af2f4a443368642799583726c94
priyanshu bharti <110742345+priyanshub3443@users.noreply.github.com>
2024-02-25 21:59:20 +0530

    insrtion tree

---
+++ dsa/instrito_in_tree.c
@@ -0,0 +1,140 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *left;
+    struct node *right;
+};
+
+struct node *createNode(int data)
+{
+    struct node *newNode = (struct node *)malloc(sizeof(struct node));
+    if (newNode == NULL)
+    {
+        printf("Memory allocatio failed \n");
+        exit(EXIT_FAILURE);
+    }
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+void inorderTReversal(struct node *root)
+{
+    if (root == NULL)
+    {
+        inorderTReversal(root->left);
+        printf("%d ", root->data);
+        inorderTReversal(root->right);
+    }
+}
+int isBST(struct node *root)
+{
+    static struct node *prev = NULL;
+    if (root != NULL)
+    {
+        if (!isBST(root->left))
+        {
+            return 0;
+        }
+        if (prev != NULL && root->data <= prev->data)
+        {
+            return 0;
+        }
+        prev = root;
+        return isBST(root->right);
+    }
+    else
+    {
+        return 1;
+    }
+}
+
+struct node * search(struct node* root, int key){
+    if(root==NULL){
+        return NULL;
+    }
+    if(key==root->data){
+        return root;
+    }
+    else if(key<root->data){
+        return search(root->left, key);
+    }
+    else{
+        return search(root->right, key);
+    }
+}
+struct node * searchIter(struct node* root, int key){
+    while(root!=NULL){
+        if(key == root->data){
+            return root;
+        }
+        else if(key<root->data){
+            root = root->left;
+        }
+        else{
+            root = root->right;
+        }
+    }
+    return NULL;
+}
+void insert(struct node * root, int key){
+    struct node *prev=NULL;
+    while (root==NULL)
+    {
+        prev=root;
+        if (key==root->data)
+        {
+            printf("Cannot inset %d, already in BST", key);
+            return;
+        }
+        else if (key<root->data)
+        {
+            root=root->left;
+        }
+        else{
+            root=root->right;
+        }
+        
+        
+    }
+    struct node*new=createNode(key);
+    if (key<prev->data)
+    {
+        prev->left=new;
+    }
+    else{
+        prev->right=new;
+    }
+    
+    
+    
+}
+
+int main(){
+     
+    // Constructing the root node - Using Function (Recommended)
+    struct node *p = createNode(5);
+    struct node *p1 = createNode(3);
+    struct node *p2 = createNode(6);
+    struct node *p3 = createNode(1);
+    struct node *p4 = createNode(4);
+    // Finally The tree looks like this:
+    //      5
+    //     / \
+    //    3   6
+    //   / \
+    //  1   4  
+
+    // Linking the root node with left and right children
+    p->left = p1;
+    p->right = p2;
+    p1->left = p3;
+    p1->right = p4;
+
+    insert(p, 16);
+    printf("%d", p->right->right->data);
+    return 0;
+}
---
+++ dsa/search.c
@@ -0,0 +1,113 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *left;
+    struct node *right;
+};
+
+struct node *createNode(int data)
+{
+    struct node *newNode = (struct node *)malloc(sizeof(struct node));
+    if (newNode == NULL)
+    {
+        printf("Memory allocatio failed \n");
+        exit(EXIT_FAILURE);
+    }
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+void inorderTReversal(struct node *root)
+{
+    if (root == NULL)
+    {
+        inorderTReversal(root->left);
+        printf("%d ", root->data);
+        inorderTReversal(root->right);
+    }
+}
+int isBST(struct node *root)
+{
+    static struct node *prev = NULL;
+    if (root != NULL)
+    {
+        if (!isBST(root->left))
+        {
+            return 0;
+        }
+        if (prev != NULL && root->data <= prev->data)
+        {
+            return 0;
+        }
+        prev = root;
+        return isBST(root->right);
+    }
+    else
+    {
+        return 1;
+    }
+}
+
+struct node * search(struct node* root, int key){
+    if(root==NULL){
+        return NULL;
+    }
+    if(key==root->data){
+        return root;
+    }
+    else if(key<root->data){
+        return search(root->left, key);
+    }
+    else{
+        return search(root->right, key);
+    }
+}
+struct node * searchIter(struct node* root, int key){
+    while(root!=NULL){
+        if(key == root->data){
+            return root;
+        }
+        else if(key<root->data){
+            root = root->left;
+        }
+        else{
+            root = root->right;
+        }
+    }
+    return NULL;
+}
+
+int main(){
+     
+    // Constructing the root node - Using Function (Recommended)
+    struct node *p = createNode(5);
+    struct node *p1 = createNode(3);
+    struct node *p2 = createNode(6);
+    struct node *p3 = createNode(1);
+    struct node *p4 = createNode(4);
+    // Finally The tree looks like this:
+    //      5
+    //     / \
+    //    3   6
+    //   / \
+    //  1   4  
+
+    // Linking the root node with left and right children
+    p->left = p1;
+    p->right = p2;
+    p1->left = p3;
+    p1->right = p4;
+
+    struct node* n = search(p, 6);
+    if(n!=NULL){
+    printf("Found: %d", n->data);
+    }
+    else{
+        printf("Element not found");
+    }
+    return 0;
+}
